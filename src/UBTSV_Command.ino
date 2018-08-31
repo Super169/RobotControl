4458c9a9e93e7ff8618d0f81dd4efd8178c01671
James MA <Super169@126.com>
2018-08-31 16:56:43 +0800

    Can execute UBT Servo command with return to netowrk / serial

--- src/UBTSV_Command.ino
+++ src/UBTSV_Command.ino
@@ -7,6 +7,8 @@
 #pragma region "UBTech Command"
 bool UBTSV_Command() {	
 
+	if (debug) DEBUG.print(F("UBT Servo Command detected\n"));
+
 	// if (cmdBuffer.available() < 10) return false;
 	// It's not logicaly that 10 byte cannot be received within 1 ms, so consider as invalid input directly
 	if (cmdBuffer.available() < 10) {
@@ -40,19 +42,43 @@ bool UBTSV_Command() {
 
 	if (!enable_UBTSV) return true;
 
-	// TODO: how to make a generic command???
-	int size = 0;
-	if (size > 0) {
-		// if(client.connected())client.write(result,cnt);
-		// else Serial.write(result, cnt);
-		if (SWFM.wifiClientConnected()) {
-			SWFM.write(result, size);
-		} else {
-			Serial.write(result, size);
-		}
+	if (debug) DEBUG.print(F("UBT Servo Command sent\n"));
+	robotPort.enableTx(true); 
+	robotPort.write(cmd, 10);
+	robotPort.enableTx(false); 
+
+    unsigned long endMs = millis() + UBT_COMMAND_WAIT_TIME;
+    while ( (millis() < endMs) && (!robotPort.available()) ) delay(1);
+    if (!robotPort.available()) {
+		return true;
 	}
 
-	if (debug) DEBUG.print(F("UBT Servo Command executed\n"));
+	byte cnt = 0;
+	byte buffer[20];
+	while (robotPort.available()) {
+		buffer[cnt++] = robotPort.read();
+		if (cnt >= 20) break;
+		// wait 1 ms for serial data to make sure all related result received
+		if (!robotPort.available()) delay(1);
+	}
+	// Clear buffer
+	while (robotPort.available()) {
+		robotPort.read();
+		if (!robotPort.available()) delay(1);
+	}
+	
+	String sender = "Serial";
+	if (cnt > 0) {
+		if (SWFM.wifiClientConnected()) {
+			SWFM.write(buffer, cnt);
+			sender = "Network";
+		} else {
+			Serial.write(buffer, cnt);
+		}
+		if (debug) DEBUG.printf("UBT Servo Command result returned via %s\n", sender.c_str());
+	}
+
 	return true;
+
 }
 #pragma endregion
